c54b0f7ef3a4c4187df75493498815e2ead62a43
Robert McCreary Mannino <fooyinfighter@gmail.com>
2024-10-12 07:59:43 -0700

    step 2: create push function

--- week-2-stacks/stack.js
+++ week-2-stacks/stack.js
@@ -10,10 +10,17 @@ class Stack {
 
   // FOR Stack
   // push, pop
-
+  push(item) {
+    this.storage.push(item);
+  }
 
 
 }
 
 const s = new Stack();
+s.push("Alice's Journal");
+s.push("Bob's Journal");
+s.push("Carol's Journal");
+s.push("Derek's Journal");
+
 console.log(s);
