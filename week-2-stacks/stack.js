ffeab48b31e5d136f4c535ce44e2d5592729784a
Robert McCreary Mannino <fooyinfighter@gmail.com>
2024-10-12 07:58:14 -0700

    finish step 1, create Stack class

---
+++ week-2-stacks/stack.js
@@ -0,0 +1,19 @@
+
+class Stack {
+
+  constructor() {
+    this.storage = [];
+  }
+
+  // FOR Queue
+  // enqueue, dequeue
+
+  // FOR Stack
+  // push, pop
+
+
+
+}
+
+const s = new Stack();
+console.log(s);
