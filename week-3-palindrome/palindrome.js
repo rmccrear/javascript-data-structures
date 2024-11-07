cff4d303c959f5acd81e7d3df34fb1ee1ff1413a
anyoneout <chrisdafur@gmail.com>
2024-11-06 18:30:32 -0600

    added palindrome

--- week-3-palindrome/palindrome.js
+++ week-3-palindrome/palindrome.js
@@ -1,33 +1,73 @@
 // TODO: Create Stack and Queue classes.
 
-class Queue {
 
+class Queue {
+  constructor() {
+    this.storage = []; // Array to store queue elements
+  }
+
+  // Add a new element to the queue
+  enqueue(element) {
+    this.storage.push(element);
+  }
+
+  // Remove and return the first element from the queue
+  dequeue() {
+    return this.storage.shift();
+  }
+
+  // Check if the queue is empty
+  isEmpty() {
+    return this.storage.length === 0;
+  }
+
+  // Return the current size of the queue
+  size() {
+    return this.storage.length;
+  }
 }
 
 class Stack {
 
-}
+  constructor() {
+    this.storage = [];
+  }
+  push(element) {
+    this.storage.push(element);
+  }
+  pop() {
+    return this.storage.pop();
+  }
+  peek() {
+    return this.storage[this.storage.length - 1];
+  }
+  isEmpty() {
+    return this.storage.length === 0;
+  }
+};
+
 
 function isPalindrome(str) {
   // TODO: instantiate the stack and queue
-  let stack ;
-  let queue ;
+  let stack = new Stack();
+  let queue = new Queue();
 
   // TODO: Step 1: Sanitize the input
-  let sanitizedStr; 
+  let sanitizedStr = str; 
 
   // TODO: Step 2: Push characters onto stack and enqueue them into queue
   for (let i=0; i< sanitizedStr.length; i++) {
     let char = sanitizedStr[i]; 
 
-
+    stack.push(char);
+    queue.enqueue(char);
 
   }
 
   // TODO: Step 3: Compare characters by popping from stack and dequeuing from queue
-  while (!stack.isEmpty()) {
-    let a;
-    let b;
+  while (!stack.isEmpty() && !queue.isEmpty()) {
+    let a = stack.pop();
+    let b = queue.dequeue();
 
       if (a !== b) {
           return "The string is not a palindrome.";
@@ -38,5 +78,5 @@ function isPalindrome(str) {
 }
 
 // Example usage:
-let inputStr = prompt("Enter a string to check for palindrome: ");
+let inputStr = "tacaascat";
 console.log(isPalindrome(inputStr));
